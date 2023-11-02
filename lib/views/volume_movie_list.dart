1a9969cca76a7ead41b9b8b222c7edf3b87581b4
Zach Freeman <zsfree00@gmail.com>
2023-11-02 11:02:59 -0400

    Rework VolumeMovieList such that it is a StatefulWidget

--- lib/views/volume_movie_list.dart
+++ lib/views/volume_movie_list.dart
@@ -7,42 +7,58 @@ Future<List<GreatMovies>> fetchMovies(int volumeNumber) async {
   return await database.moviesForVolume(volumeNumber);
 }
 
-class VolumeMovieList extends StatelessWidget {
+class VolumeMovieList extends StatefulWidget {
   final int volume;
   const VolumeMovieList({super.key, required this.volume});
+
+  @override
+  State<VolumeMovieList> createState() => _VolumeMovieListState();
+}
+
+class _VolumeMovieListState extends State<VolumeMovieList> {
+  List<GreatMovies> greatMovies = [];
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isLoading = true;
+    fetchMovies(widget.volume).then((result) {
+      setState(() {
+        isLoading = false;
+        greatMovies = result;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var title = 'Volume $volume';
-    return FutureBuilder(
-      future: fetchMovies(volume),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
+    var title = 'Volume ${widget.volume}';
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: isLoading
+          ? const Center(
               child: SizedBox(
-            width: 40.0,
-            height: 40.0,
-            child: CircularProgressIndicator(),
-          ));
-        }
-
-        if (snapshot.hasData) {
-          return MaterialApp(
-            title: title,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-              useMaterial3: true,
-            ),
-            home: Scaffold(
+              width: 40.0,
+              height: 40.0,
+              child: CircularProgressIndicator(),
+            ))
+          : Scaffold(
               appBar: AppBar(title: Text(title)),
               body: ListView.builder(
                   padding: const EdgeInsets.all(8),
-                  itemCount: snapshot.data.length,
+                  itemCount: greatMovies.length,
                   itemBuilder: (context, index) {
-                    final greatMovie = snapshot.data[index];
+                    final greatMovie = greatMovies[index];
                     return ListTile(
                       title: Text(greatMovie.name),
                       trailing: const Icon(Icons.visibility),
-                      iconColor: greatMovie.isWatched ? Colors.blue : Colors.grey,
+                      iconColor:
+                          greatMovie.isWatched ? Colors.blue : Colors.grey,
                       subtitle: Text(greatMovie.director),
                       onTap: () {
                         Navigator.push(
@@ -54,11 +70,6 @@ class VolumeMovieList extends StatelessWidget {
                     );
                   }),
             ),
-          );
-        }
-
-        return Container();
-      },
     );
   }
 }
