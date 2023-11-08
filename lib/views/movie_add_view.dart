1a8c1065e66421f2a061b62337c5052d5a60bfd8
Zach Freeman <zsfree00@gmail.com>
2023-11-08 10:22:43 -0500

    Add MovieAddView star rating logic

--- lib/views/movie_add_view.dart
+++ lib/views/movie_add_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_great_movies/models/great_movie_model.dart';
 import 'package:intl/intl.dart';
@@ -14,6 +15,9 @@ class MovieAddView extends StatefulWidget {
 
 class _MovieAddViewState extends State<MovieAddView> {
   DateTime selectedDate = DateTime.now();
+  double selectedRating = 0.0;
+  static const double outerHorizontalPadding = 0.0;
+  static const double innerHorizontalPadding = 25.0;
 
   // #docregion styles
   final nameTextStyle = const TextStyle(
@@ -51,8 +55,8 @@ class _MovieAddViewState extends State<MovieAddView> {
     fontSize: 16,
     height: 2,
   );
-
   // #enddocregion styles
+
   Future updateMovie(BuildContext context, String id) async {
     var database = Provider.of<GreatMovieDatabase>(context, listen: false);
     return await database.updateMovie(id);
@@ -62,16 +66,24 @@ class _MovieAddViewState extends State<MovieAddView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text("Cancel"),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 5),
+          child: TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text("Cancel"),
+          ),
         ),
-        leadingWidth: 80,
+        leadingWidth: 100,
         title: const Text("I Watched..."),
         centerTitle: true,
-        actions: [TextButton(onPressed: () {}, child: const Text("Save"))],
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 25),
+            child: TextButton(onPressed: () {}, child: const Text("Save")),
+          )
+        ],
       ),
       body: Center(
           child: Column(children: [
@@ -87,11 +99,12 @@ class _MovieAddViewState extends State<MovieAddView> {
 
   Widget movieInfo(GreatMovies greatMovie) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
+      padding: const EdgeInsets.symmetric(horizontal: outerHorizontalPadding),
       child: ColoredBox(
           color: Colors.grey.shade100,
           child: Padding(
-              padding: const EdgeInsets.all(25),
+              padding: const EdgeInsets.symmetric(
+                  horizontal: innerHorizontalPadding, vertical: 25),
               child: Row(
                 children: [
                   Text(greatMovie.name, style: nameTextStyle),
@@ -104,11 +117,12 @@ class _MovieAddViewState extends State<MovieAddView> {
 
   Widget dateWatched(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
+      padding: const EdgeInsets.symmetric(horizontal: outerHorizontalPadding),
       child: ColoredBox(
           color: Colors.white,
           child: Padding(
-              padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+              padding: const EdgeInsets.symmetric(
+                  horizontal: innerHorizontalPadding, vertical: 10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -121,11 +135,26 @@ class _MovieAddViewState extends State<MovieAddView> {
 
   Widget datePicker(BuildContext context) {
     return TextButton(
-        child: Text(DateFormat('EEEE, MMMM d, yyyy').format(selectedDate), style: valueTextStyle),
+        child: Text(DateFormat('EEEE, MMMM d, yyyy').format(selectedDate),
+            style: valueTextStyle),
         onPressed: () => _selectDate(context));
   }
 
   _selectDate(BuildContext context) async {
+    final ThemeData theme = Theme.of(context);
+    switch (theme.platform) {
+      case TargetPlatform.android:
+      case TargetPlatform.fuchsia:
+      case TargetPlatform.linux:
+      case TargetPlatform.windows:
+      case TargetPlatform.macOS:
+        return buildMaterialDatePicker(context);
+      case TargetPlatform.iOS:
+        return buildCupertinoDatePicker(context);
+    }
+  }
+
+  buildMaterialDatePicker(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
         context: context,
         initialDate: selectedDate,
@@ -138,32 +167,91 @@ class _MovieAddViewState extends State<MovieAddView> {
     }
   }
 
+  buildCupertinoDatePicker(BuildContext context) async {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext builder) {
+          return Container(
+            height: MediaQuery.of(context).copyWith().size.height / 3,
+            color: Colors.white,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              onDateTimeChanged: (picked) {
+                if (picked != selectedDate) {
+                  setState(() {
+                    selectedDate = picked;
+                  });
+                }
+              },
+              initialDateTime: selectedDate,
+              minimumYear: 2000,
+              maximumYear: 2025,
+            ),
+          );
+        });
+  }
+
   // #docregion ratings, stars
-  final stars = Row(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.green[500]),
-      const Icon(Icons.star, color: Colors.black),
-      const Icon(Icons.star, color: Colors.black),
-    ],
-  );
+  Widget stars() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        IconButton(
+            onPressed: () {
+              _setRating(1.0);
+            },
+            icon: Icon(Icons.star,
+                color:
+                    selectedRating > 0.0 ? Colors.green[500] : Colors.black)),
+        IconButton(
+            onPressed: () {
+              _setRating(2.0);
+            },
+            icon: Icon(Icons.star,
+                color:
+                    selectedRating > 1.0 ? Colors.green[500] : Colors.black)),
+        IconButton(
+            onPressed: () {
+              _setRating(3.0);
+            },
+            icon: Icon(Icons.star,
+                color:
+                    selectedRating > 2.0 ? Colors.green[500] : Colors.black)),
+        IconButton(
+            onPressed: () {
+              _setRating(4.0);
+            },
+            icon: Icon(Icons.star,
+                color:
+                    selectedRating > 3.0 ? Colors.green[500] : Colors.black)),
+      ],
+    );
+  }
+
+  _setRating(double rating) {
+    setState(() {
+      selectedRating = rating;
+    });
+  }
 
   // #enddocregion stars
   Widget rated() {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
+      padding: const EdgeInsets.symmetric(horizontal: outerHorizontalPadding),
       child: ColoredBox(
           color: Colors.white,
           child: Padding(
-              padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+              padding: const EdgeInsets.symmetric(
+                  horizontal: innerHorizontalPadding, vertical: 10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [Text("Rated", style: entryTypeTextStyle), stars],
+                    children: [
+                      Text("Rated", style: entryTypeTextStyle),
+                      stars()
+                    ],
                   )
                 ],
               ))),
@@ -172,11 +260,12 @@ class _MovieAddViewState extends State<MovieAddView> {
 
   Widget addReview() {
     return const Padding(
-        padding: EdgeInsets.fromLTRB(25, 0, 25, 0),
+        padding: EdgeInsets.symmetric(horizontal: outerHorizontalPadding),
         child: ColoredBox(
             color: Colors.white,
             child: Padding(
-                padding: EdgeInsets.fromLTRB(25, 10, 25, 10),
+                padding: EdgeInsets.symmetric(
+                    horizontal: innerHorizontalPadding, vertical: 10),
                 child: Column(
                   children: [
                     TextField(
