62347030abcb4cbad300a114488e1fca82c5e937
Zach Freeman <zsfree00@gmail.com>
2023-11-15 14:17:39 -0500

    Add "You watched this film" row to MovieListItem

--- lib/views/movie_list_item.dart
+++ lib/views/movie_list_item.dart
@@ -34,49 +34,42 @@ class _MovieListItemState extends State<MovieListItem> {
         title: Text(widget.pageTitle),
       ),
       body: _getBody(context, movieListItemViewModel),
-      floatingActionButton: FloatingActionButton(
-        elevation: 10.0,
-        child: const Icon(Icons.add),
-        onPressed: () {
-          Navigator.of(context).push(CupertinoPageRoute(
-              fullscreenDialog: true,
-              builder: (context) => MovieAddView(
-                  greatMovie: widget.greatMovie,
-                  posterImageUrl: movieListItemViewModel.posterImageUrl)));
-        },
-      ),
+      floatingActionButton: _getWatchFab(movieListItemViewModel),
     );
   }
 
   Widget _getBody(
       BuildContext context, MovieListItemViewModel movieListItemViewModel) {
-    final posterImageUrl = movieListItemViewModel.posterImageUrl;
     if (movieListItemViewModel.loading) {
       return const CircularProgressIndicator();
     }
     return Column(children: [
-      Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(widget.greatMovie.name, style: const TextStyle(fontSize: 16)),
-            const SizedBox(height: 20),
-            Text("${widget.greatMovie.year} | DIRECTED BY",
-                style: const TextStyle(fontSize: 12)),
-            const SizedBox(height: 5),
-            Text(widget.greatMovie.director,
-                style: const TextStyle(fontSize: 14)),
-          ],
-        ),
-        Column(
-          children: [
-            CachedNetworkImage(
-                placeholder: (context, url) =>
-                    const CircularProgressIndicator(),
-                imageUrl: posterImageUrl),
-          ],
-        )
-      ]),
+      Container(
+          padding: const EdgeInsets.all(25),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(widget.greatMovie.name,
+                    style: const TextStyle(fontSize: 16)),
+                const SizedBox(height: 20),
+                Text("${widget.greatMovie.year} | DIRECTED BY",
+                    style: const TextStyle(fontSize: 12)),
+                const SizedBox(height: 5),
+                Text(widget.greatMovie.director,
+                    style: const TextStyle(fontSize: 14)),
+              ],
+            ),
+            Column(
+              children: [
+                CachedNetworkImage(
+                    placeholder: (context, url) =>
+                        const CircularProgressIndicator(),
+                    imageUrl: movieListItemViewModel.posterImageUrl),
+              ],
+            )
+          ])),
       Row(
         children: [
           Expanded(
@@ -84,7 +77,50 @@ class _MovieListItemState extends State<MovieListItem> {
                   padding: const EdgeInsets.all(25),
                   child: Text(movieListItemViewModel.description)))
         ],
-      )
+      ),
+      _watchInfo(),
     ]);
+  }
+
+  Widget _watchInfo() {
+    if (widget.greatMovie.isWatched) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: ColoredBox(
+            color: Colors.grey.shade100,
+            child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    const Text("You watched this film"),
+                    const SizedBox(width: 5),
+                    Icon(Icons.star, color: widget.greatMovie.userStarRating > 0.0 ? Colors.green.shade400 : Colors.grey.shade100),
+                    Icon(Icons.star, color: widget.greatMovie.userStarRating > 1.0 ? Colors.green.shade400 : Colors.grey.shade100),
+                    Icon(Icons.star, color: widget.greatMovie.userStarRating > 2.0 ? Colors.green.shade400 : Colors.grey.shade100),
+                    Icon(Icons.star, color: widget.greatMovie.userStarRating > 3.0 ? Colors.green.shade400 : Colors.grey.shade100),
+                  ],
+                ))),
+      );
+    }
+    return Container();
+  }
+
+  Widget _getWatchFab(MovieListItemViewModel movieListItemViewModel) {
+    if (!widget.greatMovie.isWatched) {
+      return FloatingActionButton(
+        elevation: 10.0,
+        child: const Icon(Icons.remove_red_eye),
+        onPressed: () {
+          Navigator.of(context).push(CupertinoPageRoute(
+              fullscreenDialog: true,
+              builder: (context) => MovieAddView(
+                  greatMovie: widget.greatMovie,
+                  posterImageUrl: movieListItemViewModel.posterImageUrl)));
+        },
+      );
+    }
+    return Container();
   }
 }
